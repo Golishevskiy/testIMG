1e0d95ac563a7479ebdae123366d4ea0848f50ec
Petro <golishevskiypetro@ukr.net>
2020-12-14 20:27:44 +0200

    Initial Commit

---
+++ testIMG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testIMG
+//
+//  Created by Petro GOLISHEVSKIY on 14.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
